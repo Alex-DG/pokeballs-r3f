85ad2b8863e38dfbc812771b13588a58731810ce
Alex Di Guida <alexandre.diguida@gmail.com>
2022-01-29 12:58:19 +0000

    Removed commented code

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,6 @@ import { useGLTF, Environment } from '@react-three/drei'
 import { EffectComposer, DepthOfField } from '@react-three/postprocessing'
 import * as THREE from 'three'
 
-// function Banana({ z }) {
-//   const ref = useRef()
-//   const { nodes, materials } = useGLTF('/banana-v1-transformed.glb')
-//   const { viewport, camera } = useThree()
-//   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z])
-
-//   const [data] = useState({
-//     x: THREE.MathUtils.randFloatSpread(2),
-//     y: THREE.MathUtils.randFloatSpread(height),
-//     rX: Math.random() * Math.PI,
-//     rY: Math.random() * Math.PI,
-//     rZ: Math.random() * Math.PI,
-//   })
-
-//   useFrame((state) => {
-//     ref.current.rotation.set(
-//       (data.rX += 0.001),
-//       (data.rY += 0.001),
-//       (data.rZ += 0.001)
-//     )
-
-//     ref.current.position.set(data.x * width, (data.y += 0.025), z)
-
-//     if (data.y > height) data.y = -height
-//   })
-
-//   return (
-//     <mesh
-//       ref={ref}
-//       geometry={nodes.bananas.geometry}
-//       material={materials.skin}
-//       material-emissive='orange'
-//     />
-//   )
-// }
-
 function Pokeball({ z }) {
   const ref = useRef()
   const { nodes, materials } = useGLTF('/pokeball-v2-transformed.glb')
